521ce8955e9bfaa8d7102de824fa18bf0928d6a1
NA-Aray <nirwan.arrachman@aray.ma>
2024-09-26 00:00:04 +0700

    change API

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./payload";
 
 // const baseUrl = "https://backend-production-f9e7.up.railway.app/api/v1/"
-const baseUrl = "http://ascent-api.cloud/api/v1/";
+const baseUrl = "https://start-production.up.railway.app/api/v1/";
 
 const api = axios.create({
   baseURL: baseUrl,
